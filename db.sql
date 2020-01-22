fedec732847095b45a37d110c431ab7def00760f
chiachi <charles@6e.com.tw>
2020-01-22 22:41:54 +0800

    刪除部分使得無法 select 的字元

--- db.sql
+++ db.sql
@@ -3,21 +3,21 @@ use gobelieve;
 
 
 CREATE TABLE `group` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `appid` bigint(20) DEFAULT NULL,
-  `master` bigint(20) DEFAULT NULL,
-  `super` tinyint(4) NOT NULL DEFAULT '0',
-  `name` varchar(255) DEFAULT NULL,
-  `notice` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  id bigint(20) NOT NULL AUTO_INCREMENT,
+  appid bigint(20) DEFAULT NULL,
+  master bigint(20) DEFAULT NULL,
+  super tinyint(4) NOT NULL DEFAULT 0,
+  name varchar(255) DEFAULT NULL,
+  notice varchar(255) DEFAULT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `group_member` (
-  `group_id` bigint(20) NOT NULL DEFAULT '0',
-  `uid` bigint(20) NOT NULL DEFAULT '0',
-  `nickname` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`group_id`,`uid`),
-  KEY `idx_group_member_uid` (`uid`)
+CREATE TABLE group_member (
+  group_id bigint(20) NOT NULL DEFAULT 0,
+  uid bigint(20) NOT NULL DEFAULT 0,
+  nickname varchar(255) DEFAULT NULL,
+  PRIMARY KEY (group_id,uid),
+  KEY idx_group_member_uid (uid)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
